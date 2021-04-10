59250c3444483321cf2436fbdd25dfd150fecc7c
SouravSinghRana <41117593+SouravSinghRana@users.noreply.github.com>
2021-04-10 17:41:18 +0530

    all files uploaded

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { useState } from "react";
+import "./style.css";
 
-function App() {
+import Header from "./components/Header";
+import CreateArea from "./components/CreateArea";
+import Note from "./components/Note";
+import Footer from "./components/Footer";
+
+function App(props) {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes((prevValue) => {
+      return [...prevValue, newNote];
+    });
+  }
+
+  function deleteNotes(id) {
+    setNotes((preValue) => {
+      return [...preValue.filter((note, index) => index !== id)];
+    });
+  }
   return (
-    <div>
-      Hello
+    <div className="App">
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((note, index) => (
+        <Note
+          key={index}
+          id={index}
+          title={note.title}
+          content={note.content}
+          onDelete={deleteNotes}
+        />
+      ))}
+      <Footer />
     </div>
   );
 }
